0c6cd9e260c353b0ae34ed73efa5f2b9e7abdaf0
JCastilloRC <jamel.castillo@endava.com>
2023-02-06 19:56:09 -0500

    Primer Challenge

--- src/challenge1_ComputerScience/DanceFloor.java
+++ src/challenge1_ComputerScience/DanceFloor.java
@@ -1,5 +1,46 @@
 package challenge1_ComputerScience;
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
+
 
 public class DanceFloor {
+	
+	private int[][] danceFloorPeople;
+	
+	private int danceFloorLength;
+	
+	public DanceFloor(String filePath){
+     
+		setDanceFloorPeople(filePath);
+		
+    }
+	
+	private void setDanceFloorPeople(String filePath) {
+		int rows = -1;
+		try {
+		      File textFile = new File(filePath);
+		      Scanner myReader = new Scanner(textFile);
+		      while (myReader.hasNextLine()) {
+		    	if(rows == -1) {
+		    		danceFloorLength = Integer.parseInt(myReader.nextLine());
+		    		danceFloorPeople = new int[danceFloorLength][danceFloorLength];
+		    	}
+		    	else {
+		    		 String fileData = myReader.nextLine();
+		    		 String[] fileIntegers = fileData.split("	");
+		    		 for( int columns = 0; columns < danceFloorLength; columns++) {
+		    			 danceFloorPeople[rows][columns] = Integer.parseInt(fileIntegers[columns]);
+		    	      }
+		    	}
+		    	rows++;
+		      }
+		      myReader.close(); 
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }		
+	}
+
 
 }
--- src/challenge1_ComputerScience/Main.java
+++ src/challenge1_ComputerScience/Main.java
@@ -1,12 +1,14 @@
 package challenge1_ComputerScience;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 // 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
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-
+		
+		DanceFloor piso = new DanceFloor("prueba1.txt");
 	}
-
 }
