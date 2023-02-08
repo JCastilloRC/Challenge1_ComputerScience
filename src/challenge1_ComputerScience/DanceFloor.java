e72d6d9491db2b26997e7fdafa7452a97724891b
JCastilloRC <jamel.castillo@endava.com>
2023-02-08 00:57:23 -0500

    Primer Challenge

--- src/challenge1_ComputerScience/DanceFloor.java
+++ src/challenge1_ComputerScience/DanceFloor.java
@@ -6,30 +6,27 @@ import java.util.Scanner;
 public class DanceFloor {
 	
 	private int[][] danceFloorPeople;
-	private int[][] lengthPaths;
-	private String[][] stringPaths;
 	private int danceFloorLength;
-	private int longestPath;
+	private int longestPath = 0;
 	private String longestStringPath;
 	
 	public DanceFloor(String filePath){
 		setDanceFloorPeople(filePath);	
-		setAgePaths();
-		setMaxValues();	
+		setLongestPath();
     }
 	
 	private void setDanceFloorPeople(String filePath) {
 		int rows = -1;
 		try {
 		      File textFile = new File(filePath);
-		      Scanner myReader = new Scanner(textFile);
-		      while (myReader.hasNextLine()) {
+		      Scanner reader = new Scanner(textFile);
+		      while (reader.hasNextLine()) {
 		    	if(rows == -1) {
-		    		danceFloorLength = Integer.parseInt(myReader.nextLine());
+		    		danceFloorLength = Integer.parseInt(reader.nextLine());
 		    		danceFloorPeople = new int[danceFloorLength][danceFloorLength];
 		    	}
 		    	else {
-		    		 String fileData = myReader.nextLine();
+		    		 String fileData = reader.nextLine();
 		    		 String[] fileIntegers = fileData.split(" ");
 		    		 for(int columns = 0; columns < danceFloorLength; columns++) {
 		    			 danceFloorPeople[rows][columns] = Integer.parseInt(fileIntegers[columns]);
@@ -37,58 +34,51 @@ public class DanceFloor {
 		    	}
 		    	rows++;
 		      }
-		      myReader.close(); 
+		      reader.close(); 
 		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");		      
 		    }		
 	}
 
-	private void setAgePaths() {
-		lengthPaths = new int[danceFloorLength][danceFloorLength];
-		stringPaths = new String[danceFloorLength][danceFloorLength];
+	private void setLongestPath() {
+		int [][]lengthPaths = new int[danceFloorLength][danceFloorLength];
+		String[][]stringPaths = new String[danceFloorLength][danceFloorLength];
 		for (int rows = danceFloorLength - 1; rows >= 0; rows--) {
 			for (int columns = danceFloorLength - 1; columns >= 0; columns--) {				
+				int currentPath = 0;
+				int rightPath = 0;
+				int downPath = 0;
 				lengthPaths[rows][columns] = 1;
-				int right = 0;
-				int down = 0;
 				try {
 					if(danceFloorPeople[rows][columns] == danceFloorPeople[rows][columns + 1] + 1 || danceFloorPeople[rows][columns] == danceFloorPeople[rows][columns + 1] - 1 ) {
-						right = lengthPaths[rows][columns + 1];
+						rightPath = lengthPaths[rows][columns + 1];
 					}
 				}catch(ArrayIndexOutOfBoundsException e){
-					right = 0;
+					rightPath = 0;
 				}
 				try {
 					if(danceFloorPeople[rows][columns] == danceFloorPeople[rows + 1][columns] + 1 || danceFloorPeople[rows][columns] == danceFloorPeople[rows + 1][columns] - 1 ) {
-						down = lengthPaths[rows + 1][columns];
+						downPath = lengthPaths[rows + 1][columns];
 					}
 				}catch(ArrayIndexOutOfBoundsException e){
-					down = 0;
+					downPath = 0;
 				}
-				if(right > down) {
+				if(rightPath > downPath) {
 					lengthPaths[rows][columns] += lengthPaths[rows][columns + 1];
 					stringPaths[rows][columns] = Integer.toString(danceFloorPeople[rows][columns]) + " - " + stringPaths[rows][columns + 1];
 				}
-				else if(down > right) {
+				else if(downPath > rightPath) {
 					lengthPaths[rows][columns] += lengthPaths[rows + 1][columns];
 					stringPaths[rows][columns] = Integer.toString(danceFloorPeople[rows][columns]) + " - " + stringPaths[rows + 1][columns];
 				}
-				else if(down == right && down + right != 0){
+				else if(downPath == rightPath && downPath + rightPath != 0){
 					lengthPaths[rows][columns] += lengthPaths[rows][columns + 1];
 					stringPaths[rows][columns] = Integer.toString(danceFloorPeople[rows][columns]) + " - " + stringPaths[rows][columns + 1];
 				}
 				else {
 					stringPaths[rows][columns] = Integer.toString(danceFloorPeople[rows][columns]);
-				}		
-			}
-		}
-	}
-
-	private void setMaxValues() {
-		longestPath = 1;
-		for(int rows = 0; rows < danceFloorLength - 1; rows++) {
-			for(int columns = 0; columns < danceFloorLength - 1; columns++) {
-				int currentPath = lengthPaths[rows][columns];
+				}
+			    currentPath = lengthPaths[rows][columns];
 				if(currentPath > longestPath) {
 					longestPath = currentPath;
 					longestStringPath = stringPaths[rows][columns];
