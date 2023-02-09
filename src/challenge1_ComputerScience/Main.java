47c789dd041b85de1be07087e1c09c2e20435129
JCastilloRC <jamel.castillo@endava.com>
2023-02-08 19:19:44 -0500

    Primer Challenge

--- src/challenge1_ComputerScience/Main.java
+++ src/challenge1_ComputerScience/Main.java
@@ -5,15 +5,10 @@ public class Main {
 		
 		boolean isOptimized = true;
 		String textPath = "DanceFloor03.txt";
-		
-		long initialTime = System.nanoTime();
 		DanceFloor endavaParty = new DanceFloor(textPath, isOptimized);
-		long finalTime = System.nanoTime();
 		System.out.print("Longest Endavans Line Dance is: ");
 		System.out.println(endavaParty.getLongestStringPath());
 		System.out.print("Length of Path is: ");
 		System.out.println(endavaParty.getLongestPath());
-		System.out.print("Elapsed Time is: ");
-		System.out.println(finalTime - initialTime);
 	}
 }
